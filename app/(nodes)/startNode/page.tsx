0dbda8081f4596da9379368013be79dc997231f7
Aswatth <aswatth56@gmail.com>
2025-07-03 10:51:58 +0530

    Added connection limit to START node's handle.

--- app/(nodes)/startNode/page.tsx
+++ app/(nodes)/startNode/page.tsx
@@ -1,14 +1,18 @@
 import styles from "./page.module.css";
-import { Handle, Position } from "@xyflow/react";
+import { Handle, Position, useNodeConnections } from "@xyflow/react";
 
 export default function StartNode() {
+  const connections = useNodeConnections({
+    handleType: "source",
+  });
+  
   return (
     <div className={styles.node}>
       <h3>START</h3>
       <Handle
         type="source"
         position={Position.Right}
-        isConnectable={true}
+        isConnectable={connections.length < 1}
         style={{ width: "12px", height: "12px" }}
       />
     </div>
